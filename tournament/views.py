98a6d2ab5482f6c8dff4d0d352d3c0ce02803fa3
Jeevan K <jeevank2865@gmail.com>
2026-02-17 22:13:15 +0530

    Update views.py

--- tournament/views.py
+++ tournament/views.py
@@ -65,11 +65,10 @@ def dashboard(request):
             'tournament': tournament,
         })
     elif role == 'referee':
-    tournament = Tournament.objects.order_by('id').first()
-    return render(request, 'tournament/referee_dashboard.html', {
+        tournament = Tournament.objects.first()
+        return render(request, 'tournament/referee_dashboard.html', {
         'tournament': tournament
     })
-    
     else:
         tournaments = Tournament.objects.all()
         return render(request, 'tournament/guest_dashboard.html', {'tournaments': tournaments})
@@ -440,6 +439,4 @@ def create_referee(request):
             return redirect('dashboard')
     else:
         form = CustomUserCreationForm()
-
     return render(request, 'tournament/create_referee.html', {'form': form})
-
